9fe988c06fb8ed50c9f9d820907ec5dd3071b025
Bob <andrew@risertech.com>
2026-01-26 15:31:41 -0700

    feat(characters): add src field to Character model and migrate

--- controllers/mapsController.js
+++ controllers/mapsController.js
@@ -1,5 +1,62 @@
-const getMaps = (req, res) => {
-  res.json({ maps: [] });
-};
+import { prisma } from "../lib/prisma.js";
 
-export { getMaps };
+const getAllMaps = async (req, res) => {
+  try {
+    const maps = await prisma.map.findMany()
+
+    return res.status(200).json({ maps })
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({ error })
+  }
+}
+
+const getSingleMap = async (req, res) => {
+  const { mapId } = req.params
+
+  try {
+    const map = await prisma.map.findUnique({
+      where: {
+        id: Number(mapId)
+      },
+      include: {
+        characters: true
+      }
+    })
+
+    return res.status(200).json({ map })
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({ error })
+  }
+}
+
+const checkCoordinates = async (req, res) => {
+  const { mapId } = req.params
+  const { char, x, y } = req.query
+
+  try {
+    const character = await prisma.character.findFirst({
+      where: {
+        mapId: Number(mapId),
+        name: char
+      }
+    })
+
+    if (Math.abs(x - character.x) <= 2 && Math.abs(y - character.y) <= 3) {
+      return res.status(200).json({ success: true, name: char })
+    }
+
+    return res.status(200).json({ success: false, name: char })
+
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({ error })
+  }
+}
+
+export {
+  getAllMaps,
+  getSingleMap,
+  checkCoordinates
+}
--- routes/maps.js
+++ routes/maps.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { getMaps } from '../controllers/mapsController.js';
+import * as controller from '../controllers/mapsController.js';
 
 const router = express.Router();
 
-router.get('/', getMaps);
+router.get('/', controller.getAllMaps);
+router.get('/:mapId/onClick', controller.checkCoordinates);
+router.get('/:mapId', controller.getSingleMap);
 
 export default router;
